612829d6fb49e4a49edcd762056170255e859889
inzilion <inzilion@gmail.com>
2023-06-20 10:36:11 +0900

    css 분리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import Header from './component/Header';
+import Body from './component/Body';
+import Footer from './component/Footer';
 
 function App() {
   return (
---
+++ src/component/Body.js
@@ -0,0 +1,12 @@
+import "./Body.css";
+
+const Body = () => {
+  return ( 
+      <div className="body">
+        <h1>Body </h1>
+      </div>
+    );
+
+}
+
+export default Body;
